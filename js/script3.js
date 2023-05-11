9ca5281c2842f0d2eb06156283e7fa0022e60e5d
chaquito7 <nicolsgonza765@gmail.com>
2023-05-11 18:13:21 -0300

    version 1.2

--- js/script3.js
+++ js/script3.js
@@ -1,63 +1,120 @@
-const tareas = [
+const formulario = document.getElementById('form-1')
+const inputTitulo = document.getElementById('nome')
+const textArea = document.getElementById('textarea')
+const btnReset = document.querySelector('#form-1 button[type="reset"]')
+const div = document.getElementById('div-cartas')
+const busqueda = document.getElementById('search')
+const checkbox = document.getElementById('check-apple')
+
+let notas = [
     {
-        id: 1 ,
-        titulo: 'pasear al perro',
-        texto: 'sacar a pasearlo',
-        realizada: false
+        id: 1,
+        titulo: "comer",
+        descripcion: "hoy tengo que comer",
+        realizado: false
     },
     {
-        id: 2 ,
-        titulo: 'cocinar',
-        texto: 'cocinar antes que llegue la familia',
-        realizada: false
-    }, 
+        id: 2,
+        titulo: "jugar",
+        descripcion: "hoy tengo que comer",
+        realizado: false
+    }
+        
 ]
+pintarDiv(notas, div)
 
-let idGlobal = tareas[1].id
+//primera parte
 
-let contenedor = document.getElementById("div-general")
+let idGeneral = 3
 
-function maquetador(objeto) {
-   return `<div class="card">
-    <h2>${objeto.titulo}</h2>
-    <p>${objeto.texto}</p>
-    <button onclick="borrarNota(${objeto.id})">borrar nota</button>
-    </div>`
-}
-
-function pintar(lista , ubicacion){
-    for(let elemento of lista){
-        ubicacion.innerHTML += maquetador(elemento)
-    } 
-}
-
-pintar(tareas , contenedor)
-
-function crearNotas(titulo, texto) {
-    return `<div class="card">
-    <h2>${titulo}</h2>
-    <p>${texto}</p>
-    <button onclick="borrarNota(${idGlobal + 1})">borrar nota</button>
-    </div>`
-}
-
-const formulario = document.querySelector("#formulario")
-
-formulario.addEventListener ("submit", (e)=>{
+formulario.addEventListener('submit', (e)=>{
     e.preventDefault()
-    let titulo = formulario[0].value;
-    let text = formulario[1].value;
-    tareas.push({id: idGlobal + 1,
-         titulo: titulo,
-          texto: text,
-           realizada: false});
-    contenedor.innerHTML = "";
-    pintar(tareas, contenedor);
+    if(inputTitulo.value && textArea.value){
+        let nota = {
+            id: idGeneral++,
+            titulo: inputTitulo.value,
+            descripcion: textArea.value,
+            realizado: false
+        }
+        
+        reiniciar(inputTitulo.value, textArea.value)
+        notas.push(nota)
+        pintarDiv(notas, div)
+        
+    }
+   console.log(notas)
 })
 
-function borrarNota(id) {
-    tareas = tareas.filter(tarea => tarea.id !== id);
-    contenedor.innerHTML = "";
-    pintar(tareas, contenedor);
+btnReset.addEventListener('click', ()=>{
+    reiniciar(inputTitulo, textArea)
+})
+
+div.addEventListener('click', (e)=>{
+    let dataSet = e.target.dataset
+    if(dataSet.accion == "borrar"){
+        notas = notas.filter(nota => nota.id != dataSet.id)
+        pintarDiv(notas, div)
+    }if(dataSet.accion == "estado"){
+        const nota = notas.find(nota => nota.id == dataSet.id)
+        nota.realizado = !nota.realizado
+    }
+})
+
+busqueda.addEventListener('input', ()=>{
+    const filtrarPorBusqueda = filtrarPorTitulo(notas , busqueda.value)
+    if(checkbox.checked){
+    const filtarPorRealizada = filtrarPorRealizadas(filtrarPorBusqueda)
+    pintarDiv(filtarPorRealizada, div)
+    }else{
+        pintarDiv(filtrarPorBusqueda, div)
+    }
+})
+
+checkbox.addEventListener('change', ()=>{
+    const filtrarPorBusqueda = filtrarPorTitulo(notas , busqueda.value)
+    if (checkbox.checked){
+        const filtrarPorRealizada = filtrarPorRealizadas(filtrarPorBusqueda)
+        pintarDiv(filtrarPorRealizada, div)
+    }else{
+        pintarDiv(filtrarPorBusqueda, div)
+    }
+})
+
+
+function filtrarPorRealizadas(notas) {
+    return notas.filter(nota => nota.realizado);
+  }
+  
+
+function filtrarPorTitulo (notas, busqueda){
+    return notas.filter(nota => nota.titulo.toLowerCase().includes(busqueda.toLowerCase()))
 }
 
+
+
+
+function reiniciar(input, text){
+    text.value = ''
+    input.value = ''
+}
+
+function pintarDiv(notas , ubicacion){
+    let template = ""
+    for(let nota of notas){
+        template += crearDiv(nota)
+    }
+    ubicacion.innerHTML = template
+}
+
+function crearDiv (nota){
+    let estado = ''
+    if(nota.realizado){
+        estado = 'checked'
+    }
+    return `<div>
+         <input data-id="${nota.id}" type="checkbox" ${estado} name="chan" data-accion="estado" id="checkbox">
+         <h3>${nota.titulo}</h3>
+         <p>${nota.descripcion}</p>
+        <button type="reset" data-accion="borrar" data-id="${nota.id}">borrar</button>
+        </div>`
+}
